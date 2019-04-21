230c0c6173e4e0c6b511b8548af110adb31e0c7c
HongThuan <guest@localhost.localdomain>
2019-04-21 19:37:43 +0700

    delete

--- temp.py
+++
@@ -1,9 +0,0 @@
-def backquote(command_list):
-    for command in command_list:
-        pos = 0
-        while '$' in command[pos:]:
-            index = command.index('$', pos)
-            if command[index-1] == '\\':
-                pos = index + 1
-                continue
-            exe_command = command[pos:].partition('${')[-1].partition('}')[0]
--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ls built_ins.py intek-sh.py __pycache__ sb sdg temp.py convert.py logical.py quoting.py sc sdh globbing.py path_expandsion.py sa sd shell.py
