9b2db7260ba2789ae77249c0c9222894a6b96058
hthuan <you@example.com>
2019-04-23 12:34:47 +0700

    add

--- backquote.py
+++ backquote.py
@@ -14,13 +14,13 @@ def backquote(command_str, pos, open_char, close_char):
     try:
         if not exe_command:
             raise ValueError()
-        process_obj = run(exe_command, stdout=PIPE, encoding='utf-8')
+        process_obj = run(exe_command, stdout=PIPE)
     except ValueError:
         command_str = command_str.replace(open_char + exe_command + close_char, '')
-    except Exception:
-        print("bash: {}: command_str not found".format(exe_command))
+    except Exception as e:
+        print("bash: {}: command not found".format(exe_command))
         command_str = command_str.replace(open_char + exe_command + close_char, '')
     else:
         command_str = command_str[:pos] +\
-                    command_str[pos:].replace(open_char + exe_command + close_char, process_obj.stdout[:-1])
+                    command_str[pos:].replace(open_char + exe_command + close_char, process_obj.stdout[:-1].decode())
     return command_str
